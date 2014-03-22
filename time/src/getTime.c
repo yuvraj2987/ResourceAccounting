56d540e247f0d46161ec0e7fd5c2cd6747ea6b1c
Amit Kulkarni <amitshri@buffalo.edu>
2014-03-22 09:19:21 +0000

    c time measurement code to check pi clock resolution

---
+++ time/src/getTime.c
@@ -0,0 +1,28 @@
+
+
+#include <time.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("--- Get Time starts ----\n");
+    struct timespec first, second;
+    /*clock_gettime(CLOCK_MONOTONIC, &first);
+    clock_gettime(CLOCK_MONOTONIC, &second);
+    long diff = second.tv_nsec - first.tv_nsec;
+    printf("Time diff: %ld in nano seconds\n", diff);
+    long micro_diff = diff/1000;
+    printf("Time diff: %ld in micro seconds\n", micro_diff);
+    */
+    register short int count = 0;
+    clock_gettime(CLOCK_MONOTONIC, &first);
+    clock_gettime(CLOCK_MONOTONIC, &second);
+ 
+    while(second.tv_nsec - first.tv_nsec< 1000000)
+    {
+        clock_gettime(CLOCK_MONOTONIC, &second);
+        count++; 
+    }
+
+    printf("For 1ms count is:%d\n", count);
+}
