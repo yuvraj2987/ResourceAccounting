3b1eced87060824ea8da4723246b1b2d45c3143b
Amit Kulkarni <amitshri@buffalo.edu>
2014-04-04 04:45:43 -0400

    measurement log

---
+++ python_module/measurement/server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import socket
+
+def main():
+  print "--- starting server ---"
+  s = socket.socket()
+  host = socket.gethostname()
+  port = 12345
+  s.bind((host, port))
+
+  s.setblocking(1)
+  s.listen(5)
+ 
+  while True:
+    c, addr = s.accept()
+    print "Got connection from:", addr
+#    c.send("Thank you for connecting to the server")
+#    print "sent, now receiving..."
+    mystr = c.recv(1024)
+    print "got this:"
+    print mystr
+    print "reception done, closing"
+    c.close()
+
+if __name__== '__main__':
+  main()
