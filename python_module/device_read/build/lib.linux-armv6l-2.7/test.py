fa9c13f376d18ca780fefd17e7beadcb601c12dd
Amit Kulkarni <amitshri@buffalo.edu>
2014-06-01 13:25:51 -0400

    Modified some variable names

--- python_module/device_read/build/lib.linux-armv6l-2.7/test.py
+++ python_module/device_read/build/lib.linux-armv6l-2.7/test.py
@@ -19,14 +19,15 @@ while True:
   print "%-20s\t%-20s\t%-20s\t%-20s" % ("DEVICE_NAME", "OPEN_TIME", "PID", "LATENCY")
   for dev in range(0, ndevices):
     #pName, time_sec, time_usec = deviceModule.set_device_info(fd, dev)
-    tuple = deviceModule.set_device_info(fd, dev)
-    #print "Returned value length", len(tuple)
-    deviceName = tuple[0]
-    openTime = "%ld.%06ld" % (tuple[1], tuple[2])
-    #tuple[3] tuple[4] - event generated and event dropped... tty no relation
-    pid = tuple[5]
-    eventsConsumed = tuple[6]
-    latency = "%ld.%06ld" % (tuple[7], tuple[8])
+    tuples = deviceModule.set_device_info(fd, dev)
+    print "Result from set_device_info:", str(tuples)
+    print "Returned value length", len(tuples)
+    deviceName = tuples[0]
+    openTime = "%ld.%06ld" % (tuples[1], tuples[2])
+    #tuples[3] tuples[4] - event generated and event dropped... tty no relation
+    pid = tuples[5]
+    eventsConsumed = tuples[6]
+    latency = "%ld.%06ld" % (tuples[7], tuples[8])
     print "%-20s\t%-20s\t%-20hd\t%-20s" % (deviceName, openTime, pid, latency)
     print "---------------------------------------------------"
     time.sleep(15)
